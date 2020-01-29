7b793267bbeb13623550411c18ab314eb6434080
unknown <aleksi.mikael.lahti@gmail.com>
2020-01-29 16:42:44 +0200

    overlay triggering added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,100 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NewDeviceOverlayComponent } from './new-device-overlay/new-device-overlay.component';
+import { DialogOverviewExampleDialog, NewDeviceOverlayComponent} from './new-device-overlay/new-device-overlay.component';
 import { MapComponent } from './main/map/map.component';
+import { HttpClientModule } from '@angular/common/http';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTreeModule} from '@angular/material/tree';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    NewDeviceOverlayComponent,
-    MapComponent
+    NewDeviceOverlayComponent, DialogOverviewExampleDialog,
+    MapComponent,
+   
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
     MatToolbarModule,
-    BrowserAnimationsModule
+    MatTooltipModule,
+    MatTreeModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [
+    NewDeviceOverlayComponent, DialogOverviewExampleDialog
+  ],
+  bootstrap: [AppComponent, NewDeviceOverlayComponent]
 })
 export class AppModule { }
--- src/app/new-device-overlay/new-device-overlay.component.ts
+++ src/app/new-device-overlay/new-device-overlay.component.ts
@@ -1,6 +1,17 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { DeviceService } from 'src/app/device.service';
 
+export interface DialogData {
+  value: string;
+  animal: string;
+  name: string;
+  foods: [];
+}
+export interface Food {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-new-device-overlay',
   templateUrl: './new-device-overlay.component.html',
@@ -10,13 +21,21 @@ export class NewDeviceOverlayComponent implements OnInit {
   @Input() overlayOpen: boolean;
   latitude: any;
   longitude: any;
-  constructor(private deviceService: DeviceService) {}
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+  animal: string;
+  name: string;
+  value: string;
+  constructor(private deviceService: DeviceService, public dialog: MatDialog) {}
 
   ngOnInit() {
   }
   ngOnChanges(changes: SimpleChanges) {
     if(this.deviceService.overlay){
-      this.longitude = this.deviceService.longitude;
+      /*this.longitude = this.deviceService.longitude;
       this.latitude = this.deviceService.latitude;
       console.log(this.latitude, this.longitude);
       const device = {deviceType: 'soundSensor', 
@@ -24,7 +43,32 @@ export class NewDeviceOverlayComponent implements OnInit {
                       deviceId: 'installedDevicesId', 
                       locationId: 'univOulu1', 
                       coordinates: {latitude: this.latitude, longitude: this.longitude}};
-      this.deviceService.addDeviceToDatabase(device);
-    }
-}
+      this.deviceService.addDeviceToDatabase(device);*/
+      const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+        width: '250px',
+        data: {name: this.name, animal: this.animal, foods: this.foods, value: this.value}
+      });
+  
+      dialogRef.afterClosed().subscribe(result => {
+        console.log('The dialog was closed');
+        this.animal = result;
+        console.log(this.animal);
+      });
+
+    }}}
+
+@Component({
+  selector: 'new-device-overlay-dialog',
+  templateUrl: 'new-device-overlay-dialog.html',
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
